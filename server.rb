e8eecf2f78ee87ab70b5246c3a70dd662f2f95e2
Francisco Natale <francisco_natale@hotmail.com>
2025-05-10 10:06:00 -0300

    minor changes

--- server.rb
+++ server.rb
@@ -1,9 +1,22 @@
+require 'bundler/setup'
 require 'sinatra/base'
+require 'sinatra/reloader' if Sinatra::Base.environment == :development
+require 'logger'
 
-# Define una clase 'App' que hereda de 'Sinatra::Application',
-# convirtiéndola en una aplicación web Sinatra.
 class App < Sinatra::Application
-  # Define una ruta para solicitudes GET a la URL raíz ('/').
+
+  configure :development do
+    enable :logging
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::DEBUG if development?
+    set :logger, logger
+
+    register Sinatra::Reloader
+    after_reload do
+      logger.info 'Reloaded!!!'
+    end
+  end
+
   get '/' do
     'Welcome'
   end
