6363902c6d235d60cb417513bb175f2c66b6570b
maxinperez <maxi2017p@gmail.com>
2025-05-19 12:52:06 -0300

    an account has many transactions and their origins

--- models/account.rb
+++ models/account.rb
@@ -1,8 +1,8 @@
 class Account < ActiveRecord::Base
   
   belongs_to :user, foreign_key: 'dni', primary_key: 'dni'
-  #has_many : transactions
-
+  #contiene varias transacciones y sus origenes. 
+  has_many :source_transactions, class_name: 'Transaction', foreign_key: 'source_account_id'
   
   validates :account_number, presence: true, uniqueness: { message: "ya está en uso por otra cuenta" }
   validates :alias, presence: true, uniqueness: { message: "ya está en uso por otra cuenta" }
