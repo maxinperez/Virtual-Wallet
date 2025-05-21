7ba2187c57c6a4a854d1cf6fea999bac4b8095c4
agusc546 <aguscesari14@gmail.com>
2025-05-21 11:58:37 -0300

    modified Gemfile and transaction.rb

--- models/transaction.rb
+++ models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ActiveRecord::Base
-  belongs_to :source_account, class_name :'Account'
-  belongs_to :target_account, class_name :'Account'
+  belongs_to :source_account, class_name: 'Account'
+  belongs_to :target_account, class_name: 'Account'
 
   self.primary_key = 'id_transaction'
   enum estado: {
@@ -12,5 +12,14 @@ class Transaction < ActiveRecord::Base
   validates :monto, presence: true, numericality  { greater_than: 0 }
   validates :estado, presence: true, 
 
+  # Callback para generar ID de transacción si es necesario
+  before_create :generate_transaction_id, unless: :id_transaction?
+
+  private
+
+  # Crea un id random a la hora de insertar en la tabla
+  def generate_transaction_id
+    self.id_transaction = SecureRandom.uuid if self.has_attribute?(:id_transaction)
+  end
 
 end
