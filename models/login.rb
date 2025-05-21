79b8e2650f98c2cb168e8396ecf830186944896a
agusc546 <aguscesari14@gmail.com>
2025-05-21 12:14:48 -0300

    modified login.rb

--- models/login.rb
+++ models/login.rb
@@ -1,3 +1,19 @@
+# login.rb
+require 'bcrypt'
+
 class Login < ActiveRecord::Base
-    belongs_to :user, foreign_key: 'dni', primary_key: 'dni'
+  include BCrypt
+
+  belongs_to :user, foreign_key: 'dni', primary_key: 'dni'
+
+  validates :dni, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  def password=(new_password)
+    self.password_digest = Password.create(new_password)
+  end
+
+  def authenticate(password)
+    Password.new(password_digest) == password
+  end
 end
