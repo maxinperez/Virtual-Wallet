9d1ec2112eae68af6dbfeea4f9bf2e47b214c43b
agusc546 <aguscesari14@gmail.com>
2025-05-21 12:02:04 -0300

    transaction modified

--- models/transaction.rb
+++ models/transaction.rb
@@ -3,14 +3,14 @@ class Transaction < ActiveRecord::Base
   belongs_to :target_account, class_name: 'Account'
 
   self.primary_key = 'id_transaction'
-  enum estado: {
-    pendiente: 0,
-    completada: 1,
-    rechazada: 2
+  enum state: {
+    pending: 0,
+    completed: 1,
+    rejected: 2
   }
 
-  validates :monto, presence: true, numericality  { greater_than: 0 }
-  validates :estado, presence: true, 
+  validates :amount, presence: true, numericality  { greater_than: 0 }
+  validates :state, presence: true, 
 
   # Callback para generar ID de transacción si es necesario
   before_create :generate_transaction_id, unless: :id_transaction?
