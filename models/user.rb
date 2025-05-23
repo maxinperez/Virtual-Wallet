0412c6bdc39dc95717dd6b4c852c06f37fdd40b9
Tomás Rosselot <totorosselot1@gmail.com>
2025-05-22 22:21:46 -0300

    Starting with bankaccount model and view

--- models/user.rb
+++ models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   has_one :login, foreign_key: 'dni', primary_key: 'dni'
   has_one :account, foreign_key: 'dni', primary_key: 'dni'
+  has_one :bank_account, foreign_key: 'dni', primary_key: 'dni'
+
 
   validates :dni, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
