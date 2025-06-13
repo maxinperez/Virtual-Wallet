e3aa8aab07cd27c189b99c7f94bcb0d144b2c7f8
Francisco Natale <francisco_natale@hotmail.com>
2025-06-13 15:41:19 -0300

    add migration of card

---
+++ db/migrate/20250613165915_create_card.rb
@@ -0,0 +1,12 @@
+class CreateCard < ActiveRecord::Migration[8.0]
+  def change
+      create_table :cards do |t|
+        t.string :holder_name
+        t.string :card_number
+        t.string :cvv
+        t.integer :exp_month  # solo el mes
+        t.integer :exp_year   # solo el año
+        t.timestamps
+      end
+    end
+  end
