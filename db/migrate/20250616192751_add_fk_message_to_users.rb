866b64fc44d50b9673967d96ecd59366bf939db7
Francisco Natale <francisco_natale@hotmail.com>
2025-06-16 22:22:15 -0300

    fix migration

--- db/migrate/20250616192751_add_fk_message_to_users.rb
+++ db/migrate/20250616192751_add_fk_message_to_users.rb
@@ -1,6 +1,6 @@
 class AddFkMessageToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_foreign_key :messages, :accounts
+    add_foreign_key :messages, :accounts, column: :user_id
     add_index :messages, :user_id
   end
 end
