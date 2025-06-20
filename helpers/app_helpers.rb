9ac8d664e8a61763670e8f063ae16ce2dfe2e7e5
Your Name <you@example.com>
2025-06-20 14:22:37 -0300

    add helpers for show transactions

--- helpers/app_helpers.rb
+++ helpers/app_helpers.rb
@@ -22,5 +22,71 @@ module AppHelpers
     def admin?
       current_user && !current_user.account.admin.nil?
     end
+    def transaction_display_data(transaction)
+    case transaction.transaction_type
+    when "deposit"
+      sign = '+'
+      owner = "Depósito"
+      amount_display = "#{transaction.amount}"
+      case transaction.state
+      when "success"
+        amount_class = "amount-positive"
+        type = "finalizado"
+      when "rejected"
+        amount_class = "amount-rejected"
+        type = "rechazado"
+      when "pending"
+        amount_class = "amount-pending"
+        type = "pendiente"
+      else
+        amount_class = "amount-positive"
+        type = "Desconocido"
+      end
+    when "withdrawal"
+      sign = '-'
+      owner = "Retiro"
+      amount_display = "#{transaction.amount}"
+      case transaction.state
+      when "success"
+        amount_class = "amount-negative"
+        type = "finalizado"
+      when "rejected"
+        amount_class = "amount-negative"
+        type = "rechazado"
+      when "pending"
+        amount_class = "amount-pending"
+        type = "pendiente"
+      else
+        amount_class = "amount-negative"
+        type = "Desconocido"
+      end
+    else
+      other_user = transaction.source_account&.user
+      sign = other_user&.name&.slice(0)&.upcase || "?"
+      owner = other_user ? "#{other_user.name} #{other_user.last_name}" : "Usuario desconocido"
+      amount_display = "#{transaction.amount}"
+      type = "Transferencia"
+      case transaction.state
+      when "success"
+        amount_class = "amount-negative"
+        type = "finalizado"
+      when "rejected"
+        amount_class = "amount-rejected"
+        type = "rechazado"
+      when "pending"
+        amount_class = "amount-pending"
+        type = "pendiente"
+      else
+        amount_class = "amount-negative"
+        type = "Desconocido"
+      end
+    end
+    {
+      icon: sign,
+      name: owner,
+      type: type,
+      amount_class: amount_class,
+      amount: amount_display
+    }
   end
-  
+end
